19215092166c6b8b199d1ae7ede8d06522ea56d2
Mouin-bkr <mouinbkr@gmail.com>
2023-11-29 15:51:14 +0000

    backend initilized getProducts works

---
+++ server/database.js
@@ -0,0 +1,16 @@
+// connection to database.
+import mongoose from "mongoose";
+
+const connectToDatabase = async () =>{
+try {
+    mongoose.set('strictQuery',false);
+    const connect = await mongoose.connect(process.env.MONGO_URI);
+
+  console.log(`Mongo DB conencted:${connect.connection.host} `);  
+} catch (error) {
+    console.log(`Error:${error.message}`)
+}
+
+   
+};
+export default connectToDatabase;
